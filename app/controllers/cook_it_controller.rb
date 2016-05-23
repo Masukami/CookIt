22fb84d316988461a2a605313c4dfb4e03c6e94e
Masukami <suhailqamil@gmail.com>
2016-05-23 18:11:32 +0800

    Added Navigation For User/Guest

--- app/controllers/cook_it_controller.rb
+++ app/controllers/cook_it_controller.rb
@@ -7,11 +7,12 @@ class CookItController < ApplicationController
 
   def profile
   	#grab username from url as :id
-  	if (User.find_by_username(params[:id]) 
+  	#if (User.find_by_username(params[:id]) 
   		@username = params[:id]
-  	else
+  	#else
   		#redirect to root if user not found
-  		redirect_to root_path, :notice => "User not found"
+  	#	redirect_to root_path, :notice => "User not found"
+  	#end
   end
 
   def explore
